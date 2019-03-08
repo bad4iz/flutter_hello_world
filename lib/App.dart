137060a7990f7868b4f2df1f036c4196c0aad90f
bad4iz <bad4iz@gmail.com>
2019-03-08 21:40:43 +0400

    dec

--- lib/App.dart
+++ lib/App.dart
@@ -1,43 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-class MyWidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Text('my text');
-  }
-
-}
-
-class NewBox extends StatelessWidget {
-  final String _title;
-  final String _text;
-  String _imageUrl;
-
-  NewBox (this._title, this._text, {String imageUrl}) {
-    _imageUrl = imageUrl;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if(_imageUrl != null && _imageUrl != '') return Container(
-      color: Colors.black12,
-      height: 100.0,
-      child: new Row(children: <Widget>[
-        new Image.network(_imageUrl, width: 100.0, height: 100.0, fit: BoxFit.cover,),
-        Expanded ( child:  new Container(
-          padding: new EdgeInsets.all(10.0),
-          child: new Column(children: <Widget>[
-            new Text(_title, style: new TextStyle(fontSize: 20.0), overflow: TextOverflow.ellipsis,),
-            Expanded ( child: new Text(_text, softWrap: true, textAlign: TextAlign.justify,))
-          ],),
-        )
-    )
-      ],),
-    );
-  }
-
-}
+import 'package:flutter_hello_world/NewBox.dart';
 
 
 class App extends StatelessWidget {
---
+++ lib/NewBox.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NewBox extends StatelessWidget {
+  final String _title;
+  final String _text;
+  String _imageUrl;
+
+  NewBox (this._title, this._text, {String imageUrl}) {
+    _imageUrl = imageUrl;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_imageUrl != null && _imageUrl != '') return Container(
+      color: Colors.black12,
+      height: 100.0,
+      child: new Row(children: <Widget>[
+        new Image.network(_imageUrl, width: 100.0, height: 100.0, fit: BoxFit.cover,),
+        Expanded ( child:  new Container(
+          padding: new EdgeInsets.all(10.0),
+          child: new Column(children: <Widget>[
+            new Text(_title, style: new TextStyle(fontSize: 20.0), overflow: TextOverflow.ellipsis,),
+            Expanded ( child: new Text(_text, softWrap: true, textAlign: TextAlign.justify,))
+          ],),
+        )
+        )
+      ],),
+    );
+  }
+
+}
